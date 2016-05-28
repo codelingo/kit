2cf4ead84fd765c8aaeee32a4dc141e761678b94
Peter Bourgon <peter@bourgon.org>
2016-05-28 09:07:44 -0400

    examples/apigateway: WIP

--- examples/apigateway/main.go
+++ examples/apigateway/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	stdlog "log"
 	"net/http"
 	"net/url"
 	"os"
@@ -17,16 +15,18 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/consul/api"
-	"github.com/opentracing/opentracing-go"
+	stdopentracing "github.com/opentracing/opentracing-go"
 	"golang.org/x/net/context"
 
 	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/examples/addsvc/client/grpc"
-	"github.com/go-kit/kit/examples/addsvc/server"
-	"github.com/go-kit/kit/loadbalancer"
-	"github.com/go-kit/kit/loadbalancer/consul"
+	"github.com/go-kit/kit/examples/addsvc"
+	addsvcgrpcclient "github.com/go-kit/kit/examples/addsvc/client/grpc"
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
+	consulsd "github.com/go-kit/kit/sd/consul"
+	"github.com/go-kit/kit/sd/lb"
 	httptransport "github.com/go-kit/kit/transport/http"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -38,77 +38,163 @@ func main() {
 	)
 	flag.Parse()
 
-	// Log domain
-	logger := log.NewLogfmtLogger(os.Stderr)
-	logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC).With("caller", log.DefaultCaller)
-	stdlog.SetFlags(0)                             // flags are handled by Go kit's logger
-	stdlog.SetOutput(log.NewStdlibAdapter(logger)) // redirect anything using stdlib log to us
+	// Logging domain.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
+		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+	}
 
 	// Service discovery domain. In this example we use Consul.
-	consulConfig := api.DefaultConfig()
-	if len(*consulAddr) > 0 {
-		consulConfig.Address = *consulAddr
+	var client consulsd.Client
+	{
+		consulConfig := api.DefaultConfig()
+		if len(*consulAddr) > 0 {
+			consulConfig.Address = *consulAddr
+		}
+		consulClient, err := api.NewClient(consulConfig)
+		if err != nil {
+			logger.Log("err", err)
+			os.Exit(1)
+		}
+		client = consulsd.NewClient(consulClient)
 	}
-	consulClient, err := api.NewClient(consulConfig)
-	if err != nil {
-		logger.Log("err", err)
-		os.Exit(1)
-	}
-	discoveryClient := consul.NewClient(consulClient)
 
 	// Context domain.
 	ctx := context.Background()
 
 	// Set up our routes.
 	//
-	// Each Consul service name maps to multiple instances of that service. We
-	// connect to each instance according to its pre-determined transport: in this
-	// case, we choose to access addsvc via its gRPC client, and stringsvc over
-	// plain transport/http (it has no client package).
+	// Each Consul service name resolves to multiple instances of that service.
+	// We connect to each instance according to its pre-determined transport: in
+	// this case, we choose to access addsvc via its gRPC client, and stringsvc
+	// over plain transport/http (as it has no client package).
 	//
-	// Each service instance implements multiple methods, and we want to map each
-	// method to a unique path on the API gateway. So, we define that path and its
-	// corresponding factory function, which takes an instance string and returns an
-	// endpoint.Endpoint for the specific method.
+	// Each service instance implements multiple methods, and we want to map
+	// each method to a unique path on the API gateway. So, we define that path
+	// and its corresponding factory function, which takes an instance string
+	// and returns an endpoint.Endpoint for the specific method.
 	//
 	// Finally, we mount that path + endpoint handler into the router.
 	r := mux.NewRouter()
-	for consulName, methods := range map[string][]struct {
-		path    string
-		factory loadbalancer.Factory
+	for consulServiceName, methods := range map[string][]struct {
+		tags        []string
+		passingOnly bool
+		path        string
+		factory     sd.Factory
 	}{
 		"addsvc": {
-			{path: "/api/addsvc/concat", factory: grpc.MakeConcatEndpointFactory(opentracing.GlobalTracer(), nil)},
-			{path: "/api/addsvc/sum", factory: grpc.MakeSumEndpointFactory(opentracing.GlobalTracer(), nil)},
+			{
+				tags:        []string{}, // you might have e.g. "prod"
+				passingOnly: true,
+				path:        "/api/addsvc/sum",
+				factory:     grpcFactory(addsvc.MakeSumEndpoint, logger),
+			},
+			{
+				tags:        []string{},
+				passingOnly: true,
+				path:        "/api/addsvc/concat",
+				factory:     grpcFactory(addsvc.MakeConcatEndpoint, logger),
+			},
 		},
 		"stringsvc": {
-			{path: "/api/stringsvc/uppercase", factory: httpFactory(ctx, "GET", "uppercase/")},
-			{path: "/api/stringsvc/concat", factory: httpFactory(ctx, "GET", "concat/")},
+			{
+				tags:        []string{},
+				passingOnly: true,
+				path:        "/api/stringsvc/uppercase",
+				factory:     httpFactory(ctx, "GET", "uppercase/"),
+			},
+			{
+				tags:        []string{},
+				passingOnly: true,
+				path:        "/api/stringsvc/concat",
+				factory:     httpFactory(ctx, "GET", "concat/"),
+			},
 		},
 	} {
 		for _, method := range methods {
-			publisher, err := consul.NewPublisher(discoveryClient, method.factory, logger, consulName)
+			subscriber, err := consulsd.NewSubscriber(
+				client,
+				method.factory,
+				logger,
+				consulServiceName,
+				method.tags,
+				method.passingOnly,
+			)
 			if err != nil {
-				logger.Log("service", consulName, "path", method.path, "err", err)
+				logger.Log("service", consulServiceName, "path", method.path, "err", err)
 				continue
 			}
-			lb := loadbalancer.NewRoundRobin(publisher)
-			e := loadbalancer.Retry(*retryMax, *retryTimeout, lb)
-			h := makeHandler(ctx, e, logger)
-			r.HandleFunc(method.path, h)
+
+			balancer := lb.NewRoundRobin(subscriber)
+			endpoint := lb.Retry(*retryMax, *retryTimeout, balancer)
+			handler := makeHandler(ctx, endpoint, logger)
+			r.HandleFunc(method.path, handler)
 		}
 	}
 
-	// Mechanical stuff.
+	// Interrupt handler.
 	errc := make(chan error)
 	go func() {
-		errc <- interrupt()
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errc <- fmt.Errorf("%s", <-c)
 	}()
+
+	// HTTP transport.
 	go func() {
-		logger.Log("transport", "http", "addr", *httpAddr)
+		logger.Log("transport", "HTTP", "addr", *httpAddr)
 		errc <- http.ListenAndServe(*httpAddr, r)
 	}()
-	logger.Log("err", <-errc)
+
+	// Run!
+	logger.Log("exit", <-errc)
+}
+
+func grpcFactory(makeEndpoint func(addsvc.Service) endpoint.Endpoint, logger log.Logger) sd.Factory {
+	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
+		conn, err := grpc.Dial(instance, grpc.WithInsecure())
+		if err != nil {
+			return nil, nil, err
+		}
+
+		tracer := stdopentracing.GlobalTracer() // no-op tracer
+		service := addsvcgrpcclient.New(conn, tracer, logger)
+		endpoint := makeEndpoint(service)
+
+		// Notice that the addsvc gRPC client converts the connection to a
+		// complete addsvc, and we just throw away everything except the method
+		// we're interested in. A smarter factory would mux multiple methods
+		// over the same connection. But that would require more work to manage
+		// the returned io.Closer, e.g. reference counting. Since this is for
+		// the purposes of demonstration, we'll just keep it simple.
+
+		return endpoint, conn, nil
+	}
+}
+
+// TODO(pb): -- refactoring from this line ---
+
+func httpFactory(ctx context.Context, method, path string) sd.Factory {
+	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
+		if !strings.HasPrefix(instance, "http") {
+			instance = "http://" + instance
+		}
+
+		u, err := url.Parse(instance)
+		if err != nil {
+			return nil, nil, err
+		}
+		u.Path = path
+
+		return httptransport.NewClient(
+			method,
+			u,
+			passEncode,
+			passDecode,
+		).Endpoint(), nil, nil
+	}
 }
 
 func makeHandler(ctx context.Context, e endpoint.Endpoint, logger log.Logger) http.HandlerFunc {
@@ -133,47 +219,6 @@ func makeHandler(ctx context.Context, e endpoint.Endpoint, logger log.Logger) ht
 	}
 }
 
-func makeSumEndpoint(svc server.AddService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		r := request.(io.Reader)
-		var req server.SumRequest
-		if err := json.NewDecoder(r).Decode(&req); err != nil {
-			return nil, err
-		}
-		v := svc.Sum(req.A, req.B)
-		return json.Marshal(v)
-	}
-}
-
-func makeConcatEndpoint(svc server.AddService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		r := request.(io.Reader)
-		var req server.ConcatRequest
-		if err := json.NewDecoder(r).Decode(&req); err != nil {
-			return nil, err
-		}
-		v := svc.Concat(req.A, req.B)
-		return json.Marshal(v)
-	}
-}
-
-func httpFactory(ctx context.Context, method, path string) loadbalancer.Factory {
-	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
-		var e endpoint.Endpoint
-		if !strings.HasPrefix(instance, "http") {
-			instance = "http://" + instance
-		}
-		u, err := url.Parse(instance)
-		if err != nil {
-			return nil, nil, err
-		}
-		u.Path = path
-
-		e = httptransport.NewClient(method, u, passEncode, passDecode).Endpoint()
-		return e, nil, nil
-	}
-}
-
 func passEncode(_ context.Context, r *http.Request, request interface{}) error {
 	r.Body = request.(io.ReadCloser)
 	return nil
@@ -181,10 +226,4 @@ func passEncode(_ context.Context, r *http.Request, request interface{}) error {
 
 func passDecode(_ context.Context, r *http.Response) (interface{}, error) {
 	return ioutil.ReadAll(r.Body)
-}
-
-func interrupt() error {
-	c := make(chan os.Signal)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-	return fmt.Errorf("%s", <-c)
 }
