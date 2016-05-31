b14d4015e128421f7ff3b68ffd612a95a5d9a401
Peter Bourgon <peter@bourgon.org>
2016-05-30 21:15:31 -0400

    examples/profilesvc: client

--- examples/profilesvc/endpoints.go
+++ examples/profilesvc/endpoints.go
@@ -1,10 +1,29 @@
 package profilesvc
 
 import (
-	"github.com/go-kit/kit/endpoint"
+	"net/url"
+	"strings"
+
 	"golang.org/x/net/context"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 )
 
+// Endpoints collects all of the endpoints that compose a profile service. It's
+// meant to be used as a helper struct, to collect all of the endpoints into a
+// single parameter.
+//
+// In a server, it's useful for functions that need to operate on a per-endpoint
+// basis. For example, you might pass an Endpoints to a function that produces
+// an http.Handler, with each method (endpoint) wired up to a specific path. (It
+// is probably a mistake in design to invoke the Service methods on the
+// Endpoints struct in a server.)
+//
+// In a client, it's useful to collect individually constructed endpoints into a
+// single type that implements the Service interface. For example, you might
+// construct individual endpoints using transport/http.NewClient, combine them
+// into an Endpoints, and return it to the caller as a Service.
 type Endpoints struct {
 	PostProfileEndpoint   endpoint.Endpoint
 	GetProfileEndpoint    endpoint.Endpoint
@@ -17,29 +36,243 @@ type Endpoints struct {
 	DeleteAddressEndpoint endpoint.Endpoint
 }
 
-func MakeEndpoints(s Service) Endpoints {
+// MakeServerEndpoints returns an Endpoints struct where each endpoint invokes
+// the corresponding method on the provided service. Useful in a profilesvc
+// server.
+func MakeServerEndpoints(s Service) Endpoints {
 	return Endpoints{
-		PostProfileEndpoint:   makePostProfileEndpoint(s),
-		GetProfileEndpoint:    makeGetProfileEndpoint(s),
-		PutProfileEndpoint:    makePutProfileEndpoint(s),
-		PatchProfileEndpoint:  makePatchProfileEndpoint(s),
-		DeleteProfileEndpoint: makeDeleteProfileEndpoint(s),
-		GetAddressesEndpoint:  makeGetAddressesEndpoint(s),
-		GetAddressEndpoint:    makeGetAddressEndpoint(s),
-		PostAddressEndpoint:   makePostAddressEndpoint(s),
-		DeleteAddressEndpoint: makeDeleteAddressEndpoint(s),
+		PostProfileEndpoint:   MakePostProfileEndpoint(s),
+		GetProfileEndpoint:    MakeGetProfileEndpoint(s),
+		PutProfileEndpoint:    MakePutProfileEndpoint(s),
+		PatchProfileEndpoint:  MakePatchProfileEndpoint(s),
+		DeleteProfileEndpoint: MakeDeleteProfileEndpoint(s),
+		GetAddressesEndpoint:  MakeGetAddressesEndpoint(s),
+		GetAddressEndpoint:    MakeGetAddressEndpoint(s),
+		PostAddressEndpoint:   MakePostAddressEndpoint(s),
+		DeleteAddressEndpoint: MakeDeleteAddressEndpoint(s),
 	}
 }
 
-type postProfileRequest struct {
-	Profile Profile
+// MakeClientEndpoints returns an Endpoints struct where each endpoint invokes
+// the corresponding method on the remote instance, via a transport/http.Client.
+// Useful in a profilesvc client.
+func MakeClientEndpoints(instance string) (Endpoints, error) {
+	if !strings.HasPrefix(instance, "http") {
+		instance = "http://" + instance
+	}
+	tgt, err := url.Parse(instance)
+	if err != nil {
+		return Endpoints{}, err
+	}
+	tgt.Path = ""
+
+	options := []httptransport.ClientOption{}
+
+	// Note that the request encoders need to modify the request URL, changing
+	// the path and method. That's fine: we simply need to provide specific
+	// encoders for each endpoint.
+
+	return Endpoints{
+		PostProfileEndpoint:   httptransport.NewClient("POST", tgt, encodePostProfileRequest, decodePostProfileResponse, options...).Endpoint(),
+		GetProfileEndpoint:    httptransport.NewClient("GET", tgt, encodeGetProfileRequest, decodeGetProfileResponse, options...).Endpoint(),
+		PutProfileEndpoint:    httptransport.NewClient("PUT", tgt, encodePutProfileRequest, decodePutProfileResponse, options...).Endpoint(),
+		PatchProfileEndpoint:  httptransport.NewClient("PATCH", tgt, encodePatchProfileRequest, decodePatchProfileResponse, options...).Endpoint(),
+		DeleteProfileEndpoint: httptransport.NewClient("DELETE", tgt, encodeDeleteProfileRequest, decodeDeleteProfileResponse, options...).Endpoint(),
+		GetAddressesEndpoint:  httptransport.NewClient("GET", tgt, encodeGetAddressesRequest, decodeGetAddressesResponse, options...).Endpoint(),
+		GetAddressEndpoint:    httptransport.NewClient("GET", tgt, encodeGetAddressRequest, decodeGetAddressResponse, options...).Endpoint(),
+		PostAddressEndpoint:   httptransport.NewClient("POST", tgt, encodePostAddressRequest, decodePostAddressResponse, options...).Endpoint(),
+		DeleteAddressEndpoint: httptransport.NewClient("DELETE", tgt, encodeDeleteAddressRequest, decodeDeleteAddressResponse, options...).Endpoint(),
+	}, nil
 }
 
-type postProfileResponse struct {
-	Err error `json:"err,omitempty"`
+// PostProfile implements Service. Primarily useful in a client.
+func (e Endpoints) PostProfile(ctx context.Context, p Profile) error {
+	request := postProfileRequest{Profile: p}
+	response, err := e.PostProfileEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(postProfileResponse)
+	return resp.Err
 }
 
-func (r postProfileResponse) error() error { return r.Err }
+// GetProfile implements Service. Primarily useful in a client.
+func (e Endpoints) GetProfile(ctx context.Context, id string) (Profile, error) {
+	request := getProfileRequest{ID: id}
+	response, err := e.GetProfileEndpoint(ctx, request)
+	if err != nil {
+		return Profile{}, err
+	}
+	resp := response.(getProfileResponse)
+	return resp.Profile, resp.Err
+}
+
+// PutProfile implements Service. Primarily useful in a client.
+func (e Endpoints) PutProfile(ctx context.Context, id string, p Profile) error {
+	request := putProfileRequest{ID: id, Profile: p}
+	response, err := e.PutProfileEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(putProfileResponse)
+	return resp.Err
+}
+
+// PatchProfile implements Service. Primarily useful in a client.
+func (e Endpoints) PatchProfile(ctx context.Context, id string, p Profile) error {
+	request := patchProfileRequest{ID: id, Profile: p}
+	response, err := e.PatchProfileEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(patchProfileResponse)
+	return resp.Err
+}
+
+// DeleteProfile implements Service. Primarily useful in a client.
+func (e Endpoints) DeleteProfile(ctx context.Context, id string) error {
+	request := deleteProfileRequest{ID: id}
+	response, err := e.DeleteProfileEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(deleteProfileResponse)
+	return resp.Err
+}
+
+// GetAddresses implements Service. Primarily useful in a client.
+func (e Endpoints) GetAddresses(ctx context.Context, profileID string) ([]Address, error) {
+	request := getAddressesRequest{ProfileID: profileID}
+	response, err := e.GetAddressesEndpoint(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	resp := response.(getAddressesResponse)
+	return resp.Addresses, resp.Err
+}
+
+// GetAddress implements Service. Primarily useful in a client.
+func (e Endpoints) GetAddress(ctx context.Context, profileID string, addressID string) (Address, error) {
+	request := getAddressRequest{ProfileID: profileID, AddressID: addressID}
+	response, err := e.GetAddressEndpoint(ctx, request)
+	if err != nil {
+		return Address{}, err
+	}
+	resp := response.(getAddressResponse)
+	return resp.Address, resp.Err
+}
+
+// PostAddress implements Service. Primarily useful in a client.
+func (e Endpoints) PostAddress(ctx context.Context, profileID string, a Address) error {
+	request := postAddressRequest{ProfileID: profileID, Address: a}
+	response, err := e.PostAddressEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(postAddressResponse)
+	return resp.Err
+}
+
+// DeleteAddress implements Service. Primarily useful in a client.
+func (e Endpoints) DeleteAddress(ctx context.Context, profileID string, addressID string) error {
+	request := deleteAddressRequest{ProfileID: profileID, AddressID: addressID}
+	response, err := e.DeleteAddressEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(deleteAddressResponse)
+	return resp.Err
+}
+
+// MakePostProfileEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakePostProfileEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(postProfileRequest)
+		e := s.PostProfile(ctx, req.Profile)
+		return postProfileResponse{Err: e}, nil
+	}
+}
+
+// MakeGetProfileEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeGetProfileEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(getProfileRequest)
+		p, e := s.GetProfile(ctx, req.ID)
+		return getProfileResponse{Profile: p, Err: e}, nil
+	}
+}
+
+// MakePutProfileEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakePutProfileEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(putProfileRequest)
+		e := s.PutProfile(ctx, req.ID, req.Profile)
+		return putProfileResponse{Err: e}, nil
+	}
+}
+
+// MakePatchProfileEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakePatchProfileEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(patchProfileRequest)
+		e := s.PatchProfile(ctx, req.ID, req.Profile)
+		return patchProfileResponse{Err: e}, nil
+	}
+}
+
+// MakeDeleteProfileEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeDeleteProfileEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(deleteProfileRequest)
+		e := s.DeleteProfile(ctx, req.ID)
+		return deleteProfileResponse{Err: e}, nil
+	}
+}
+
+// MakeGetAddressesEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeGetAddressesEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(getAddressesRequest)
+		a, e := s.GetAddresses(ctx, req.ProfileID)
+		return getAddressesResponse{Addresses: a, Err: e}, nil
+	}
+}
+
+// MakeGetAddressEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeGetAddressEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(getAddressRequest)
+		a, e := s.GetAddress(ctx, req.ProfileID, req.AddressID)
+		return getAddressResponse{Address: a, Err: e}, nil
+	}
+}
+
+// MakePostAddressEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakePostAddressEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(postAddressRequest)
+		e := s.PostAddress(ctx, req.ProfileID, req.Address)
+		return postAddressResponse{Err: e}, nil
+	}
+}
+
+// MakeDeleteAddressEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeDeleteAddressEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(deleteAddressRequest)
+		e := s.DeleteAddress(ctx, req.ProfileID, req.AddressID)
+		return deleteAddressResponse{Err: e}, nil
+	}
+}
 
 // We have two options to return errors from the business logic.
 //
@@ -56,13 +289,15 @@ func (r postProfileResponse) error() error { return r.Err }
 // Response types that may contain business-logic errors implement that
 // interface.
 
-func makePostProfileEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(postProfileRequest)
-		e := s.PostProfile(ctx, req.Profile)
-		return postProfileResponse{Err: e}, nil
-	}
+type postProfileRequest struct {
+	Profile Profile
 }
+
+type postProfileResponse struct {
+	Err error `json:"err,omitempty"`
+}
+
+func (r postProfileResponse) error() error { return r.Err }
 
 type getProfileRequest struct {
 	ID string
@@ -75,14 +310,6 @@ type getProfileResponse struct {
 
 func (r getProfileResponse) error() error { return r.Err }
 
-func makeGetProfileEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(getProfileRequest)
-		p, e := s.GetProfile(ctx, req.ID)
-		return getProfileResponse{Profile: p, Err: e}, nil
-	}
-}
-
 type putProfileRequest struct {
 	ID      string
 	Profile Profile
@@ -93,14 +320,6 @@ type putProfileResponse struct {
 }
 
 func (r putProfileResponse) error() error { return nil }
-
-func makePutProfileEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(putProfileRequest)
-		e := s.PutProfile(ctx, req.ID, req.Profile)
-		return putProfileResponse{Err: e}, nil
-	}
-}
 
 type patchProfileRequest struct {
 	ID      string
@@ -113,14 +332,6 @@ type patchProfileResponse struct {
 
 func (r patchProfileResponse) error() error { return r.Err }
 
-func makePatchProfileEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(patchProfileRequest)
-		e := s.PatchProfile(ctx, req.ID, req.Profile)
-		return patchProfileResponse{Err: e}, nil
-	}
-}
-
 type deleteProfileRequest struct {
 	ID string
 }
@@ -130,14 +341,6 @@ type deleteProfileResponse struct {
 }
 
 func (r deleteProfileResponse) error() error { return r.Err }
-
-func makeDeleteProfileEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(deleteProfileRequest)
-		e := s.DeleteProfile(ctx, req.ID)
-		return deleteProfileResponse{Err: e}, nil
-	}
-}
 
 type getAddressesRequest struct {
 	ProfileID string
@@ -149,14 +352,6 @@ type getAddressesResponse struct {
 }
 
 func (r getAddressesResponse) error() error { return r.Err }
-
-func makeGetAddressesEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(getAddressesRequest)
-		a, e := s.GetAddresses(ctx, req.ProfileID)
-		return getAddressesResponse{Addresses: a, Err: e}, nil
-	}
-}
 
 type getAddressRequest struct {
 	ProfileID string
@@ -170,14 +365,6 @@ type getAddressResponse struct {
 
 func (r getAddressResponse) error() error { return r.Err }
 
-func makeGetAddressEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(getAddressRequest)
-		a, e := s.GetAddress(ctx, req.ProfileID, req.AddressID)
-		return getAddressResponse{Address: a, Err: e}, nil
-	}
-}
-
 type postAddressRequest struct {
 	ProfileID string
 	Address   Address
@@ -189,14 +376,6 @@ type postAddressResponse struct {
 
 func (r postAddressResponse) error() error { return r.Err }
 
-func makePostAddressEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(postAddressRequest)
-		e := s.PostAddress(ctx, req.ProfileID, req.Address)
-		return postAddressResponse{Err: e}, nil
-	}
-}
-
 type deleteAddressRequest struct {
 	ProfileID string
 	AddressID string
@@ -207,11 +386,3 @@ type deleteAddressResponse struct {
 }
 
 func (r deleteAddressResponse) error() error { return r.Err }
-
-func makeDeleteAddressEndpoint(s Service) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(deleteAddressRequest)
-		e := s.DeleteAddress(ctx, req.ProfileID, req.AddressID)
-		return deleteAddressResponse{Err: e}, nil
-	}
-}
--- examples/profilesvc/transport.go
+++ examples/profilesvc/transport.go
@@ -1,24 +1,34 @@
 package profilesvc
 
+// The profilesvc is just over HTTP, so we just have a single transport.go.
+
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"golang.org/x/net/context"
+
+	"net/url"
 
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 var (
+	// ErrBadRouting is returned when an expected path variable is missing.
+	// It always indicates programmer error.
 	ErrBadRouting = errors.New("inconsistent mapping between route and handler (programmer error)")
 )
 
+// MakeHTTPHandler mounts all of the service endpoints into an http.Handler.
+// Useful in a profilesvc server.
 func MakeHTTPHandler(ctx context.Context, s Service, logger log.Logger) http.Handler {
 	r := mux.NewRouter()
-	e := MakeEndpoints(s)
+	e := MakeServerEndpoints(s)
 	options := []httptransport.ServerOption{
 		httptransport.ServerErrorLogger(logger),
 		httptransport.ServerErrorEncoder(encodeError),
@@ -215,6 +225,122 @@ func decodeDeleteAddressRequest(_ context.Context, r *http.Request) (request int
 	}, nil
 }
 
+func encodePostProfileRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("POST").Path("/profiles/")
+	req.Method, req.URL.Path = "POST", url.QueryEscape("/profiles/")
+	return encodeRequest(ctx, req, request)
+}
+
+func encodeGetProfileRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("GET").Path("/profiles/{id}")
+	r := request.(getProfileRequest)
+	req.Method, req.URL.Path = "GET", url.QueryEscape("/profiles/"+r.ID)
+	return encodeRequest(ctx, req, request)
+}
+
+func encodePutProfileRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("PUT").Path("/profiles/{id}")
+	r := request.(putProfileRequest)
+	req.Method, req.URL.Path = "PUT", url.QueryEscape("/profiles/"+r.ID)
+	return encodeRequest(ctx, req, request)
+}
+
+func encodePatchProfileRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("PATCH").Path("/profiles/{id}")
+	r := request.(patchProfileRequest)
+	req.Method, req.URL.Path = "PATCH", url.QueryEscape("/profiles/"+r.ID)
+	return encodeRequest(ctx, req, request)
+}
+
+func encodeDeleteProfileRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("DELETE").Path("/profiles/{id}")
+	r := request.(deleteProfileRequest)
+	req.Method, req.URL.Path = "DELETE", url.QueryEscape("/profiles/"+r.ID)
+	return encodeRequest(ctx, req, request)
+}
+
+func encodeGetAddressesRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("GET").Path("/profiles/{id}/addresses/")
+	r := request.(getAddressesRequest)
+	req.Method, req.URL.Path = "GET", url.QueryEscape("/profiles/"+r.ProfileID+"/addresses/")
+	return encodeRequest(ctx, req, request)
+}
+
+func encodeGetAddressRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("GET").Path("/profiles/{id}/addresses/{addressID}")
+	r := request.(getAddressRequest)
+	req.Method, req.URL.Path = "GET", url.QueryEscape("/profiles/"+r.ProfileID+"/addresses/"+r.AddressID)
+	return encodeRequest(ctx, req, request)
+}
+
+func encodePostAddressRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("POST").Path("/profiles/{id}/addresses/")
+	r := request.(postAddressRequest)
+	req.Method, req.URL.Path = "POST", url.QueryEscape("/profiles/"+r.ProfileID+"/addresses/")
+	return encodeRequest(ctx, req, request)
+}
+
+func encodeDeleteAddressRequest(ctx context.Context, req *http.Request, request interface{}) error {
+	// r.Methods("DELETE").Path("/profiles/{id}/addresses/{addressID}")
+	r := request.(deleteAddressRequest)
+	req.Method, req.URL.Path = "DELETE", url.QueryEscape("/profiles/"+r.ProfileID+"/addresses/"+r.AddressID)
+	return encodeRequest(ctx, req, request)
+}
+
+func decodePostProfileResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response postProfileResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodeGetProfileResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response getProfileResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodePutProfileResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response putProfileResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodePatchProfileResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response patchProfileResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodeDeleteProfileResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response deleteProfileResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodeGetAddressesResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response getAddressesResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodeGetAddressResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response getAddressResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodePostAddressResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response postAddressResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
+func decodeDeleteAddressResponse(_ context.Context, resp *http.Response) (interface{}, error) {
+	var response deleteAddressResponse
+	err := json.NewDecoder(resp.Body).Decode(&response)
+	return response, err
+}
+
 // errorer is implemented by all concrete response types that may contain
 // errors. It allows us to change the HTTP response code without needing to
 // trigger an endpoint (transport-level) error. For more information, read the
@@ -236,6 +362,19 @@ func encodeResponse(ctx context.Context, w http.ResponseWriter, response interfa
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(response)
+}
+
+// encodeRequest likewise JSON-encodes the request to the HTTP request body.
+// Don't use it directly as a transport/http.Client EncodeRequestFunc:
+// profilesvc endpoints require mutating the HTTP method and request path.
+func encodeRequest(_ context.Context, req *http.Request, request interface{}) error {
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(request)
+	if err != nil {
+		return err
+	}
+	req.Body = ioutil.NopCloser(&buf)
+	return nil
 }
 
 func encodeError(_ context.Context, err error, w http.ResponseWriter) {
